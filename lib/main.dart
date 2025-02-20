be2ae4472008434f0da8e38747f89fabc5941ccc
Nitin-Singh18 <i.nitinsingh18@gmail.com>
2025-02-20 17:54:43 +0530

    fea: setup error handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'common/utils.dart';
 import 'providers/items_provider.dart';
 import 'screens/dashboard_screen.dart';
 import 'theme/app_theme.dart';
@@ -7,19 +10,51 @@ import 'theme/app_theme.dart';
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => ItemsProvider()..loadItems(),
-      child: const MyApp(),
-    ),
-  );
+  runZonedGuarded(() {
+    runApp(
+      ChangeNotifierProvider(
+        create: (context) => ItemsProvider()..loadItems(),
+        child: const MyApp(),
+      ),
+    );
+  }, (e, s) {
+    debugPrint("Global Error Handler Caught: ${e.toString()}");
+
+    /// Shows a bottom sheet for all uncaught errors (Both Sync and Async)
+    if (navigatorKey.currentContext != null) {
+      showErrorBottomSheet(context: navigatorKey.currentContext!, exception: e);
+    }
+  });
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String? _lastShownError;
+
+  @override
   Widget build(BuildContext context) {
+    final error =
+        context.select<ItemsProvider, String?>((selector) => selector.error);
+
+    if (error != null && error != _lastShownError) {
+      _lastShownError = error; // Store the last shown error
+
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        debugPrint("Error Handler Caught: $error");
+
+        showErrorBottomSheet(
+          context: navigatorKey.currentContext!,
+          exception: error,
+        );
+      });
+    }
+
     return MaterialApp(
       title: 'Expyr App (Assignment 1)',
       debugShowCheckedModeBanner: false,
---
+++ lib/widgets/error_bottom_sheet.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../common/extensions/context_extension.dart';
+import '../common/utils.dart';
+import '../theme/app_color.dart';
+import 'ea_elevated_button.dart';
+
+class ErrorBottomSheet extends StatelessWidget {
+  final Object exception;
+
+  const ErrorBottomSheet({super.key, required this.exception});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _getIcon(),
+          vGap(20),
+          Text(
+            exception.toString(),
+            style: context.textTheme.titleLarge!
+                .copyWith(fontWeight: FontWeight.w600),
+            textAlign: TextAlign.center,
+          ),
+          vGap(20),
+          EaElevatedButton(
+              title: "Okay", onPressed: () => Navigator.pop(context))
+        ],
+      ),
+    );
+  }
+
+  Widget _getIcon() => Container(
+        padding: const EdgeInsets.all(10),
+        decoration: const BoxDecoration(
+          color: AppColor.lightRedColor,
+          shape: BoxShape.circle,
+        ),
+        child: const Icon(
+          Icons.error_outline,
+          size: 30,
+        ),
+      );
+}
